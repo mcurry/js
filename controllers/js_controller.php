96b2a85618f82c01597d1c7e85083670899a2d03
Matt Curry <matt@pseudocoder.com>
2009-09-30 14:44:06 -0400

    updated with suggestions from oscarcarlsson

--- controllers/js_controller.php
+++ controllers/js_controller.php
@@ -1,22 +1,30 @@
 <?php
 class JsController extends JsAppController {
-  var $name = 'Js';
-  var $uses = array('Js.JsLang');
+	var $name = 'Js';
+	var $uses = array('Js.JsLang');
 
-  function lang() {
-    $cache = true;
-    if (Configure::read('debug')) {
-      $cache = false;
-    }
+	function beforeFilter() {
+		parent::beforeFilter();
+		if(!empty($this->Auth)) {
+			$this->Auth->allow('lang');
+		}
+	}
 
-    if ($js = $this->JsLang->i18n($this->params['pass'], $cache)) {
-      header('Content-type: text/javascript');
-      echo $js;
-      die;
-    } else {
-      $this->cakeError('error404');
-    }
-  }
+	function lang() {
+		$cache = true;
+		if (Configure::read('debug')) {
+			$cache = false;
+			Configure::write('debug', 0);
+		}
+
+		if ($js = $this->JsLang->i18n($this->params['pass'], $cache)) {
+			header('Content-type: text/javascript');
+			echo $js;
+			die;
+		} else {
+			$this->cakeError('error404');
+		}
+	}
 }
 
 ?>
